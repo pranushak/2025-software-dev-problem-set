94d6ca2043c5ffeed73db722262859a46b0d27cd
Pranusha Keerthipati <pranushak87@gmail.com>
2025-07-01 15:34:54 -0500

    found frequency number

--- solutions/Medium/topFrequentNumbers.js
+++ solutions/Medium/topFrequentNumbers.js
@@ -1,15 +1,17 @@
 function frequentNumbers(nums, k) {
     const result = {};
-    const resultArr = [];
+
     for(let num of nums) {
-      result[num] = (result[num] || 0) + 1;
-   }
-    
-   const sorted = Object.entries(result)
-    .sort((a,b) =>b[1] - a[1])
+        result[num] = (result[num] || 0) + 1;
+
+    }
+
+    const sortedArr = Object.entries(result)
+    .sort((a,b) => b[1] - a[1])
     .slice(0,k)
     .map(([n]) => parseInt(n))
-    return sorted;
+
+    return sortedArr;
 }
 
 let nums = [1,1,1,2,2,3]
